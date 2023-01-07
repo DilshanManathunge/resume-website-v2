ef5a86f642cbfee1c38dfbee0630a4507ae4687f
Dilshan Manathunge <51945885+DilshanManathunge@users.noreply.github.com>
2023-01-07 22:36:52 +0530

    fixing navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./navbar.css"
 const Navbar = () => {
   return (
-    <div className="fixed w-4/5 h-1/5 text-end">
+    <div className="fixed w-4/5 h-1/5 flex-end">
       <div className="py-10">
         <ol className="text-white" id="nav-items">
           <li className="pr-5">Home</li>
